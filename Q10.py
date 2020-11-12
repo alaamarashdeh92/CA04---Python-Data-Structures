e229c6e4f8e0e9d58e20fcf02fdc73d6c0c7668b
alaamarashdeh92 <74057038+alaamarashdeh92@users.noreply.github.com>
2020-11-12 12:50:03 +0200

    Add files via upload

---
+++ Q10.py
@@ -0,0 +1,12 @@
+# Q 10
+# *******
+
+first_list = [2, 3, 4, 5, 6, 7, 8]
+
+second_list = [4, 9, 16, 25, 36, 49, 64]
+# print("{")
+
+for i in range(len(first_list)):
+           print("(", first_list[i],",",second_list[i], ")" , end="," )
+
+# results_set = {(2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64)}
